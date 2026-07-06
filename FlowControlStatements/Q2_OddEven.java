eac1d97a772f38d4233be515fd27f225c252ca63
Naga Pravallika <pravallikareddyvadde@gmail.com>
2026-07-07 02:15:24 +0530

    Completed Flow Control Statements Q1-Q5

---
+++ FlowControlStatements/Q2_OddEven.java
@@ -0,0 +1,14 @@
+package FlowControlStatements;
+
+public class Q2_OddEven {
+
+    public static void main(String[] args) {
+
+        int num = Integer.parseInt(args[0]);
+
+        if (num % 2 == 0)
+            System.out.println("Even");
+        else
+            System.out.println("Odd");
+    }
+}
