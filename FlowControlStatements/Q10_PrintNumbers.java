7dd1cce8f9bae3e9f1f585608e89fb9628761be5
Naga Pravallika <pravallikareddyvadde@gmail.com>
2026-07-07 02:28:43 +0530

    Completed Flow Control Statements Q6-Q10

---
+++ FlowControlStatements/Q10_PrintNumbers.java
@@ -0,0 +1,10 @@
+package FlowControlStatements;
+
+public class Q10_PrintNumbers {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.print(i + "\t");
+        }
+    }
+}
