be0e1faf91cb8cc00613902e1ca0cd8d5af750bf
Naga Pravallika <pravallikareddyvadde@gmail.com>
2026-07-07 01:05:11 +0530

    Complete Java Architecture notes and Language Basics hands-on

---
+++ LanguageBasics/Q2_WelcomeMessage.java
@@ -0,0 +1,9 @@
+public class Q2_WelcomeMessage {
+
+    public static void main(String args[]) {
+
+        System.out.println("Welcome " + args[0]);
+
+    }
+
+}
---
+++ LanguageBasics/Q3_SumOfNumbers.java
@@ -0,0 +1,12 @@
+public class Q3_SumOfNumbers {
+
+    public static void main(String args[]) {
+
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
+
+        System.out.println("The sum of " + a + " and " + b + " is " + (a + b));
+
+    }
+
+}
