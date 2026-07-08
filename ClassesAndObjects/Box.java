46c1ebcc1360b3d5312665824ded2ab79b602bc6
Naga Pravallika <pravallikareddyvadde@gmail.com>
2026-07-08 23:03:20 +0530

    Completed ClassesAndObjects hands-on

---
+++ ClassesAndObjects/Box.java
@@ -0,0 +1,20 @@
+package ClassesAndObjects;
+
+public class Box {
+
+    double width;
+    double height;
+    double depth;
+
+    // Parameterized Constructor
+    Box(double width, double height, double depth) {
+        this.width = width;
+        this.height = height;
+        this.depth = depth;
+    }
+
+    // Method to calculate volume
+    double getVolume() {
+        return width * height * depth;
+    }
+}
---
+++ ClassesAndObjects/TestCalculator.java
@@ -0,0 +1,13 @@
+package ClassesAndObjects;
+
+public class TestCalculator {
+
+    public static void main(String[] args) {
+
+        System.out.println("Power (int) = " +
+                Calculator.powerInt(2, 5));
+
+        System.out.println("Power (double) = " +
+                Calculator.powerDouble(2.5, 3));
+    }
+}
