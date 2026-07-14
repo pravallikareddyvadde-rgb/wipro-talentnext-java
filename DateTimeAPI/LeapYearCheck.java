a80dc7b6496efc31ea81b5a8064246c2551a707d
Naga Pravallika <pravallikareddyvadde@gmail.com>
2026-07-15 00:42:49 +0530

    Completes DateTimeAPI hands-on

---
+++ DateTimeAPI/LeapYearCheck.java
@@ -0,0 +1,16 @@
+package DateTimeAPI;
+
+import java.time.LocalDate;
+
+public class LeapYearCheck {
+
+    public static void main(String[] args) {
+
+        LocalDate today = LocalDate.now();
+
+        if (today.isLeapYear())
+            System.out.println(today.getYear() + " is a Leap Year");
+        else
+            System.out.println(today.getYear() + " is Not a Leap Year");
+    }
+}
